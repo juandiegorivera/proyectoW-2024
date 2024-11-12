afc1e9ad9d67122dd9815b98879d59a67fcf2a88
juandiegorivera <juanriveradiego@gmail.com>
2024-11-12 12:27:54 -0300

    4/17 comisarias agregadas

--- src/Modules/Mapa/mapa2.tsx
+++ src/Modules/Mapa/mapa2.tsx
@@ -14,8 +14,8 @@ export default function Intro() {
     const [open, setOpen] = useState(false);
     const position2 = {lat: -38.955964, lng: -68.0994502}; //Comisarìa N° 17
     const position3 = {lat: -38.9677843, lng: -68.126897}; //Jefatura de policia 
-    const position4 = {lat: 55.54, lng: 10}; //Comisarìa N° 
-    const position5 = {lat: 55.54, lng: 10}; //Comisarìa N° 
+    const position4 = {lat: -38.9677843, lng: -68.1268976}; //Comisarìa N° 41
+    const position5 = {lat: -38.9654653, lng: -68.1908727}; //Comisarìa N° 44
     const position6 = {lat: 55.54, lng: 10}; //Comisarìa N° 
     const position7 = {lat: 55.54, lng: 10}; //Comisarìa N° 
     const position8 = {lat: 55.54, lng: 10}; //Comisarìa N° 
@@ -30,7 +30,7 @@ export default function Intro() {
     const position17 = {lat: 55.54, lng: 10}; //Comisarìa N° 
     const position18 = {lat: 55.54, lng: 10}; //Comisarìa N° 
     const position19 = {lat: 55.54, lng: 10}; //Comisarìa N° 
-    return (
+return (
         <APIProvider apiKey='AIzaSyAcOwXuYm7szYIAvaDd7mEWTWy9IZpSrYM'>
             <div style={{height: "100vh", width: "100%"}}>
                 <Map
@@ -62,3 +62,49 @@ export default function Intro() {
                             glyphColor={"#1B4965"}
                         />
                     </AdvancedMarker>
+
+                    {open && (
+                        <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+                            <p>Jefatura de Policía - telefono: 299 449-3900</p>
+                        </InfoWindow>
+                    )}
+
+
+                    // Comisarìa N°41 
+                    <AdvancedMarker position={position4} onClick={() => setOpen(true)}> 
+                        <Pin
+                            background={"#62B6CB"}
+                            borderColor={"#CAE9FF"}
+                            glyphColor={"#1B4965"}
+                        />
+                    </AdvancedMarker>
+
+                    {open && (
+                        <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+                            <p>Comisarìa N°41  - telefono: 299 443-1931</p>
+                        </InfoWindow>
+                    )}
+
+
+                    // Comisarìa N°44
+                    
+                    <AdvancedMarker position={position5} onClick={() => setOpen(true)}> 
+                        <Pin
+                            background={"#62B6CB"}
+                            borderColor={"#CAE9FF"}
+                            glyphColor={"#1B4965"}
+                        />
+                    </AdvancedMarker>
+
+                    {open && (
+                        <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+                            <p>Comisarìa N°44  - telefono: 299 444-0282</p>
+                        </InfoWindow>
+                    )}
+                </Map>
+            </div>
+        </APIProvider>
+    );
+}
+
+
