8c627be2c42a51671d8fbf1e32302f45f3a4e6b7
juandiegorivera <juanriveradiego@gmail.com>
2024-11-13 16:37:03 -0300

    14/18 arregladas

--- src/Modules/Mapa/mapa2.tsx
+++ src/Modules/Mapa/mapa2.tsx
@@ -12,23 +12,23 @@ import {
 export default function Mapeishon() {
     const position = {lat: -38.9383376, lng: -68.0687734};
     const [selectedMarker, setSelectedMarker] = useState<number | null>(null);
-    const position2 = {lat: -38.964964, lng: -68.0804502}; //Comisarìa N° 17
-    const position3 = {lat: -38.96747843, lng: -68.08897}; //Jefatura de policia 
-    const position4 = {lat: -38.9750000, lng: -68.0768976}; //Comisarìa N° 41
-    const position5 = {lat: -38.9554653, lng: -68.1608727}; //Comisarìa N° 44
-    const position6 = {lat: -38.9427594, lng: -68.1750562}; //Comisarìa N° 12
-    const position7 = {lat: -38.9233082, lng: -68.166398}; //Comisarìa N° 18
+    const position2 = {lat: -38.964964, lng: -68.0804502}; //Comisarìa N° 17 .
+    const position3 = {lat: -38.96747843, lng: -68.08897}; //Jefatura de policia . 
+    const position4 = {lat: -38.9750000, lng: -68.0768976}; //Comisarìa N° 41 .
+    const position5 = {lat: -38.9654653, lng: -68.1178727}; //Comisarìa N° 44 .
+    const position6 = {lat: -38.9530000, lng: -68.1340562}; //Comisarìa N° 12 . 
+    const position7 = {lat: -38.9336666, lng: -68.122398}; //Comisarìa N° 18 
     const position8 = {lat: -38.9334162, lng: -68.107032}; //Comisarìa N° 16
     const position9 = {lat: -38.929508, lng: -68.1001462}; //Comisarìa N° 21
     const position10 = {lat: -38.9309107, lng: -68.0865849}; //Comisarìa N° 3
-    const position11 = {lat: -38.9605517, lng: -68.0588781}; //Comisarìa N° 2
-    const position12 = {lat: -38.952847, lng: -68.0659963}; //DIRECCIÓN UNIDADES DE DETENCIÓN
+    const position11 = {lat: -38.9605517, lng: -68.0588781}; //Comisarìa N° 2 . 
+    const position12 = {lat: -38.952847, lng: -68.0659963}; //DIRECCIÓN UNIDADES DE DETENCIÓN . 
     const position13 = {lat: -38.9341943, lng: -68.0881741}; //Comisaria Barrio Islas Malvinas
-    const position14 = {lat: -38.9301889, lng: -68.0612232}; //COMISARIA CUARTA
-    const position15 = {lat: -38.9625956, lng: -68.049104}; //Policia de la Provincia de Neuquén
-    const position16 = {lat: -38.9479893, lng: -68.043191}; //Departamento de Seguridad Metropolitana
-    const position17 = {lat: -38.9521525, lng: -68.0538837}; //Comisarìa N° 1
-    const position18 = {lat: -38.9119607 , lng: -68.080777}; // Comisarìa N° 20 (parque industrial)
+    const position14 = {lat: -38.9349889, lng: -68.0735232}; //COMISARIA CUARTA .
+    const position15 = {lat: -38.9625956, lng: -68.049104}; //Policia de la Provincia de Neuquén . 
+    const position16 = {lat: -38.9479893, lng: -68.043191}; //Departamento de Seguridad Metropolitana .
+    const position17 = {lat: -38.9521525, lng: -68.0538837}; //Comisarìa N° 1 . 
+    const position18 = {lat: -38.9119607 , lng: -68.080777}; // Comisarìa N° 20 (parque industrial) . 
 return (
         <APIProvider apiKey='AIzaSyAcOwXuYm7szYIAvaDd7mEWTWy9IZpSrYM'>
             <div style={{height: "100vh", width: "100%"}}>
