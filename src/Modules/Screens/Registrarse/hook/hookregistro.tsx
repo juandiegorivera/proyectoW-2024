e472b29e470a7b7b17a73acbd156a0f68e63c4a7
Rafaparra <parra.rafael.epet20@gmail.com>
2024-11-16 18:57:44 -0300

    se añadio autenticacion de usuarios

--- src/Modules/Screens/Registrarse/hook/hookregistro.tsx
+++ src/Modules/Screens/Registrarse/hook/hookregistro.tsx
@@ -1,12 +1,15 @@
 // hookRegistro.tsx
 import { useState } from 'react';
 import { addUserToCollection } from 'src/Modules/Screens/Registrarse/hook/UsecollectionRegistro';
+import { auth } from '../../../Firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const useRegistro = () => {
     const [isRegistered, setIsRegistered] = useState(false);
 
     const registerUser = async (userData: { Nombre: string; Email: string; Contraseña: string }) => {
         try {
+            await createUserWithEmailAndPassword(auth, userData.Email, userData.Contraseña);
             await addUserToCollection(userData);
             setIsRegistered(true);
         } catch (error) {
